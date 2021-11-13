47decdd14d740ee5952daa3a9d55430838cd0966
Original-Tasty <quentin.neveu@hotmail.ca>
2021-11-12 22:48:10 -0500

    Revert "1604"
    This reverts commit d0437e8fc8eced7347258d1a4c70617885b7bd13.

--- system.sh
+++ system.sh
@@ -110,19 +110,36 @@ hide_output apt-get -y --purge autoremove
 fi
 hide_output sudo apt-get update
 
-apt_install php7.0-fpm php7.0-opcache php7.0-fpm php7.0 php7.0-common php7.0-gd \
+if [[ ("$DISTRO" == "16") ]]; then
+apt_install php7.3-fpm php7.3-opcache php7.3-fpm php7.3 php7.3-common php7.3-gd \
 php-memcache php-memcached memcached \
-php7.0-mysql php7.0-imap php7.0-cli php7.0-cgi \
+php7.3-mysql php7.3-imap php7.3-cli php7.3-cgi \
 php-pear php-auth-sasl mcrypt imagemagick libruby \
-php7.0-curl php7.0-intl php7.0-pspell php7.0-recode php7.0-sqlite3 \
-php7.0-tidy php7.0-xmlrpc php7.0-xsl memcached php-memcache \
-php-imagick php-gettext php7.0-zip php7.0-mbstring \
+php7.3-curl php7.3-intl php7.3-pspell php7.3-recode php7.3-sqlite3 \
+php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache \
+php-imagick php-gettext php7.3-zip php7.3-mbstring \
 fail2ban ntpdate python3 python3-dev python3-pip \
 curl git sudo coreutils pollinate unzip unattended-upgrades cron \
 nginx pwgen libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev \
 libkrb5-dev libldap2-dev libidn11-dev gnutls-dev librtmp-dev \
-build-essential libtool autotools-dev automake pkg-config libevent-dev bsdmainutils libssl-dev
-
+build-essential libtool autotools-dev automake pkg-config libevent-dev bsdmainutils libssl-dev \
+automake cmake gnupg2 ca-certificates lsb-release nginx certbot libsodium-dev \
+libnghttp2-dev librtmp-dev libssh2-1 libssh2-1-dev libldap2-dev libidn11-dev libpsl-dev libkrb5-dev
+else
+apt_install php7.3-fpm php7.3-opcache php7.3-fpm php7.3 php7.3-common php7.3-gd \
+php7.3-mysql php7.3-imap php7.3-cli php7.3-cgi \
+php-pear php-auth-sasl mcrypt imagemagick libruby \
+php7.3-curl php7.3-intl php7.3-pspell php7.3-recode php7.3-sqlite3 \
+php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache \
+php-imagick php-gettext php7.3-zip php7.3-mbstring \
+fail2ban ntpdate python3 python3-dev python3-pip \
+curl git sudo coreutils pollinate unzip unattended-upgrades cron \
+pwgen libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev \
+libkrb5-dev libldap2-dev libidn11-dev gnutls-dev librtmp-dev \
+build-essential libtool autotools-dev automake pkg-config libevent-dev bsdmainutils libssl-dev \
+libpsl-dev libnghttp2-dev automake cmake gnupg2 ca-certificates lsb-release nginx certbot libsodium-dev \
+libnghttp2-dev librtmp-dev libssh2-1 libssh2-1-dev libldap2-dev libidn11-dev libpsl-dev libkrb5-dev
+fi
 
 echo -e "$GREEN Done...$COL_RESET"
 echo -e " Downloading Original-Tasty YiiMP Repo...$COL_RESET"
